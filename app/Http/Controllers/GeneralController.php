0150862fab3b5ce6d395d55c1cc5af52bdf9cab2
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2021-08-29 04:10:04 +0000

    second commit

---
+++ app/Http/Controllers/GeneralController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GeneralController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('general-user.about');
+        
+        
+    }
+}
