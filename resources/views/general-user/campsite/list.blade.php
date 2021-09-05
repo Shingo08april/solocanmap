d91431abbdb52daf119ad5323f29ebcf3fc9cd78
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2021-09-05 14:10:04 +0000

    キャンプ場一覧作成途中まで

--- resources/views/general-user/campsite/list.blade.php
+++ resources/views/general-user/campsite/list.blade.php
@@ -1,12 +1,101 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>lキャンプ場一覧｜ソロキャンマップ</title>
-    </head>
-    <body>
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'キャンプ場一覧')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
         <h1>キャンプ場一覧</h1>
-    </body>
-</html>
+        <div class="row">
+            <div class="posts col-md-8 mx-auto mt-3">
+               <ul class="camp_list">
+                  <li class="camp_list-single">
+                    <div class="camp-img">
+                      <img src="" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場1</h3>
+                    <p>場所：兵庫県姫路市</p>
+                  </li>
+                  <li class="camp_list-single">
+                    <div class="camp-img">
+                      <img src="{{ asset('image/02.jpeg') }}" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場2</h3>
+                    <p>場所：兵庫県姫路市</p>
+                  </li>
+                  <li class="camp_list-single">
+                    <div class="camp-imag">
+                      <img src="" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場３</h3>
+                    <p>場所：滋賀県野洲市</p>
+                  </li>
+                  <li class="camp_list-single">
+                    <div class="camp-imag">
+                      <img src="" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場4</h3>
+                    <p>場所：滋賀県野洲市</p>
+                  </li>
+                  <li class="camp_list-single">
+                    <div class="camp-imag">
+                      <img src="" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場5</h3>
+                    <p>場所：滋賀県野洲市</p>
+                  </li>
+                  <li class="camp_list-single">
+                    <div class="camp-imag">
+                      <img src="" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場6</h3>
+                    <p>場所：滋賀県野洲市</p>
+                  </li>
+                  <li class="camp_list-single">
+                    <div class="camp-imag">
+                      <img src="" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場7</h3>
+                    <p>場所：滋賀県野洲市</p>
+                  </li>
+                  <li class="camp_list-single">
+                    <div class="camp-imag">
+                      <img src="" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場8</h3>
+                    <p>場所：滋賀県野洲市</p>
+                  </li>
+                  <li class="camp_list-single">
+                    <div class="camp-imag">
+                      <img src="" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場9</h3>
+                    <p>場所：滋賀県野洲市</p>
+                  </li>
+                  <li class="camp_list-single">
+                    <div class="camp-imag">
+                      <img src="" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場10</h3>
+                    <p>場所：滋賀県野洲市</p>
+                  </li><li class="camp_list-single">
+                    <div class="camp-imag">
+                      <img src="" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場11</h3>
+                    <p>場所：滋賀県野洲市</p>
+                  </li>
+                  <li class="camp_list-single">
+                    <div class="camp-imag">
+                      <img src="" alt="キャンプ場">
+                    </div>
+                    <h3>キャンプ場12</h3>
+                    <p>場所：滋賀県野洲市</p>
+                  </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/general-user/campsite/show.blade.php
+++ resources/views/general-user/campsite/show.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>北海道・東北のキャンプ場｜ソロキャンマップ</title>
+        <title>関西のキャンプ場｜ソロキャンマップ</title>
     </head>
     <body>
-        <h1>北海道・東北のキャンプ場</h1>
+        <h1>関西のキャンプ場</h1>
     </body>
 </html>
