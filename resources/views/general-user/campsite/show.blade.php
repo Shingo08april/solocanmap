b97c2795036dfef85bf1bead20e7364e9e6f67a2
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2021-10-13 12:53:53 +0000

    キャンプ場詳細ページの五枚の画像のレイアウトを整えた。現状右側の画像間の間隔を無くした。明日は左側の画像との大きさを統一することを目指す。

--- resources/views/general-user/campsite/show.blade.php
+++ resources/views/general-user/campsite/show.blade.php
@@ -4,25 +4,18 @@
 
 @section('content')
   <div class="container">
-    <div class="images-container">
-      <div class="left-cont">
-        <div class="single-img">
-          <img src="https://s3.ap-northeast-1.amazonaws.com/img.pepy.jp/wp-content/uploads/2018/04/03065541/shutterstock_243273610.jpg">
+    <div class="row">
+      <div class="col"><img src="https://s3.ap-northeast-1.amazonaws.com/img.pepy.jp/wp-content/uploads/2018/04/03065541/shutterstock_243273610.jpg"></div>
+      <div class="col">
+        <div class="row">
+          <div class="col-6" style="padding: 0;"><img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca"></div>
+          <div class="col-6" style="padding: 0;"><img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca"></div>
+        </div>
+        <div class="row">
+          <div class="col-6" style="padding: 0;"><img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca"></div>
+          <div class="col-6" style="padding: 0;"><img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca"></div>
         </div>
       </div>
-      <div class="right-cont">
-        <div class="single-img">
-          <img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca">
-        </div>
-        <div class="single-img">
-           <img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca">
-        </div>
-        <div class="single-img">
-          <img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca">
-        </div>
-        <div class="single-img">
-          <img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca">
-        </div>
     </div>
   </div>
 @endsection
