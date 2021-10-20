000d9a4e3e9f0f0fe46c3d914f95584b68df5511
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2021-10-20 12:57:46 +0000

    画像をアップロードする段階で画像サイズを指定できるようにする。またアップロードした画像のサムネイルを表示できるようにする。

--- resources/views/general-user/campsite/show.blade.php
+++ resources/views/general-user/campsite/show.blade.php
@@ -4,18 +4,14 @@
 
 @section('content')
   <div class="container">
-    <div class="row">
-      <div class="col p-0"><img src="https://s3.ap-northeast-1.amazonaws.com/img.pepy.jp/wp-content/uploads/2018/04/03065541/shutterstock_243273610.jpg"></div>
-      <div class="col">
-        <div class="row">
-          <div class="col-6" style="padding: 0;"><img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca"></div>
-          <div class="col-6" style="padding: 0;"><img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca"></div>
-        </div>
-        <div class="row">
-          <div class="col-6" style="padding: 0;"><img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca"></div>
-          <div class="col-6" style="padding: 0;"><img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca"></div>
-        </div>
-      </div>
+  <div class="d-flex show_images-cont">
+    <div class="w-50 overflow-hidden"><img src="https://mr59fqlw.user.webaccel.jp/img/_image.php?fname=/campsite_image/10549/img_2_26314.jpg&ts=000000&rw=1200&wm=resize&rh=630&hm=resize"></div>
+    <div class="w-50 d-flex flex-wrap">
+      <div class="w-50 h-50 overflow-hidden"><img src="https://mr59fqlw.user.webaccel.jp/img/_image.php?fname=/campsite_image/10549/img_2_26315.jpg&ts=000000&rw=1200&wm=resize&rh=630&hm=resize"></div>
+      <div class="w-50 h-50 overflow-hidden"><img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca"></div>
+      <div class="w-50 h-50 overflow-hidden"><img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca"></div>
+      <div class="w-50 h-50 overflow-hidden"><img src="https://cdn-i-media.kidsna.com/resize/setting(dw=700,da=l,q=80,of=jpg)/article/3528/5A7iWlDZ75Q9O9Rfjb6nr7flP1OMSnhrtTWG4Qk2Gdu6YnF34vE8QYdyjUJNCoZt?d=5b6aa8d3&h=9a7bce77208b5a7f5dc9deb6b8e45375fd9b243c5a6d807f87e2ab0bf7e1c4ca"></div>
     </div>
   </div>
+</div>
 @endsection
